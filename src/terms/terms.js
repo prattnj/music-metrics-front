5628cc25a14896bf59e3c84b70f777e50005ee06
pratt <prattnj@gmail.com>
2023-03-10 16:24:04 -0700

    added initial terms of service list

--- src/terms/terms.js
+++ src/terms/terms.js
@@ -5,6 +5,13 @@ export function Terms() {
     return (
         <div>
             <PrimaryInfo text="🚧Terms of Service🚧"/>
+            <div className='terms-list'>
+                1. This is term 1.<br/>
+                2. This is term 2.<br/>
+                3. This is term 3.<br/>
+                4. This is term 4.<br/>
+                5. This is term 5.
+            </div>
         </div>
     )
 }
