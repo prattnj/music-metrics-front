fa4d4a93d3b3406c48f20e03ff88e604832ed9db
pratt <prattnj@gmail.com>
2023-03-10 00:56:16 -0700

    added register form

--- src/register/register.js
+++ src/register/register.js
@@ -5,6 +5,14 @@ function Register() {
     return (
         <div>
             <PrimaryInfo text="Create a Music Metrics account"/>
+            <div className='login-input-wrapper'>
+                <input type="text" placeholder="First name" className="login-input"/>
+                <input type="text" placeholder="Last name" className="login-input"/>
+                <input type="text" placeholder="Username" className="login-input"/>
+                <input type="password" placeholder="Password" className="login-input"/>
+                <input type="password" placeholder="Confirm password" className="login-input"/>
+                <input type="text" placeholder="Email" className="login-input"/>
+            </div>
         </div>
     )
 }
