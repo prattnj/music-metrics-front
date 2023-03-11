fb5925afdd2046581a3a3cd1d827b710f3781c0a
pratt <prattnj@gmail.com>
2023-03-10 22:15:13 -0700

    added login buttons

--- src/stats/stats.js
+++ src/stats/stats.js
@@ -13,7 +13,7 @@ export function Stats() {
         return (
             <div>
                 <PrimaryInfo text="Stats central."/>
-                <LoginButton text="LOGIN TO SPOTIFY" onclick={authenticate}/>
+                <LoginButton text="LOGIN TO SPOTIFY" onClick={() => authenticate}/>
             </div>
         )
     }
@@ -23,7 +23,7 @@ export function Stats() {
 export function LoginButton(props) {
     return (
         <div className='login-button-wrapper'>
-            <div className='login-button' onClick={() => props.onclick}>
+            <div className='login-button'>
                 <b>{props.text}</b>
             </div>
         </div>
